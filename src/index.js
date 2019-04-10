41e5f5613872ba0064005513c8d17fb11b7b4b0d
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-10 18:40:04 +0200

    Develepping MaskRwPhone feacture and its test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-/* eslint-disable no-console */
-const sayHi = name => `Hi ${name}`;
+// regex that checks if value contains digits only
+const digitsOnly = /^\d+$/;
 
-const message = sayHi('Jean dAmour');
+// Function to mask digits into Rw phone formatt
+const maskRWPhone = (phone) => {
+  if (!phone || typeof phone !== 'string') return null;
 
-console.log(message);
+  // Returning the untouched value when it contains none
+  if (!digitsOnly.test(phone) || phone.length !== 12) return phone;
+
+  // returning the masked value
+  const codeArea = phone.substring(0, 3);
+  const prefix = phone.substring(3, 6);
+  const subPrefix = phone.substring(6, 9);
+  const sufix = phone.substring(9, 12);
+
+  return `(+${codeArea}) ${prefix}-${subPrefix}-${sufix}`;
+};
+
+export default maskRWPhone;
--- test/index.js
+++ test/index.js
@@ -1,9 +1,18 @@
-import assert from 'assert';
+import * as assert from 'assert';
+import maskRWPhone from '../src/index';
+
+const testSamples = [
+  { input: 'abc', expectedResult: 'abc', description: 'should return pristine value when receiving "abc"' },
+  { input: 'abc123456712', expectedResult: 'abc123456712', description: 'should return pristine value when receiving "abc123456712"' },
+  { input: 'abcdefghijsd', expectedResult: 'abcdefghijsd', description: 'should return pristine value when receiving "abcdefghijsd"' },
+  { input: '123456789012', expectedResult: '(+123) 456-789-012', description: 'should return (+123) 456-789-012' },
+  { input: '543126098723', expectedResult: '(+543) 126-098-723', description: 'should return (+543) 126-098-723' },
+];
 
 describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('Should return -1 when value is not presente', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+  testSamples.forEach((sample) => {
+    it(sample.description, () => {
+      assert.equal(maskRWPhone(sample.input), sample.expectedResult);
     });
   });
 });
